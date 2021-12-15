f0bb4954590e2c0689055bfe6e8dbfc9d010a166
Flavio Franco <flavio.win@gmail.com>
2021-12-15 06:46:29 -0300

    Instalando bootstrap no front

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+    <div>
+      <Button>Ola mundo</Button>
     </div>
   );
 }
