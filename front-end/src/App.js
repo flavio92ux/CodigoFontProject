6d5c028b238d8a07961d965d4c8deb59928e7458
Flavio Franco <flavio.win@gmail.com>
2021-12-15 10:40:07 -0300

    FEAT: criando tela de registro de usuario

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={ <Navigate replace to="/login" /> } />
       <Route path="/login" element={ <LoginPage /> } />
+      <Route path="/register" element={ <RegisterPage /> } />
     </Routes>
   );
 }
--- front-end/src/pages/RegisterPage.jsx
+++ front-end/src/pages/RegisterPage.jsx
@@ -4,17 +4,20 @@ import { useNavigate } from 'react-router-dom';
 import fetchLogin from '../services/fetchLoginAndRegister';
 import inputsVerification from '../utils/InputsVerification';
 
-function LoginPage() {
+function RegisterPage() {
   const [disabled, setDisabled] = useState(true);
-  const [email, setEmail] = useState('flavio_php@hotmain.com');
-  const [password, setPassword] = useState('12345678');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConf, setPasswordConf] = useState('');
   const [invalidLogin, setInvalidLogin] = useState(false);
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    setDisabled(!inputsVerification(email, password));
-  }, [email, password]);
+    if (password === passwordConf) {
+      setDisabled(!inputsVerification(email, password));
+    }
+  }, [email, password, passwordConf]);
 
   const emailInput = useRef(null);
 
@@ -27,10 +30,11 @@ function LoginPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const data = await fetchLogin(email, password, 'login');
+    const data = await fetchLogin(email, password, 'register');
     const { token, message } = data;
 
     if (message) setInvalidLogin(message);
+
     if (token) {
       setInvalidLogin(false);
       localStorage.setItem('token', token);
@@ -67,24 +71,27 @@ function LoginPage() {
         />
       </Form.Group>
 
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password Confirmation"
+          value={ passwordConf }
+          onChange={ (e) => setPasswordConf(e.target.value) }
+        />
+      </Form.Group>
+
       <Button
         variant="primary"
         type="submit"
         disabled={ disabled }
       >
-        Login
-      </Button>
-
-      <Button
-        variant="primary"
-        type="button"
-        onClick={ () => navigate('/register') }
-      >
         Register
       </Button>
+
       { invalidLogin && <span>{ invalidLogin }</span> }
     </Form>
   );
 }
 
-export default LoginPage;
+export default RegisterPage;
